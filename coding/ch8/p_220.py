2a833ea458074e16aafaf8e62b32ee0596a28266
jghee <ghj171937@gmail.com>
2021-09-08 17:22:28 +0900

    dp

--- coding/ch8/p_220.py
+++ coding/ch8/p_220.py
@@ -7,5 +7,18 @@ for i in range(0, n):
         dp[i] = max(dp[i], a[i] + a[j])
         dp[j] = dp[i]
 
-print(dp)
-print(max(dp))
+# print(dp)
+# print(max(dp))
+#
+#
+# n = int(input())
+# array = list(map(int, input().split()))
+#
+# d = [0] * 100
+#
+# d[0] = array[0]
+# d[1] = max(array[0], array[1])
+# for i in range(2, n):
+#     d[i] = max(d[i - 1], d[i - 2] + array[i])
+#
+# print(d[n-1])
