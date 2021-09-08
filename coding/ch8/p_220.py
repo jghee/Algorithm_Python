6d894c1f5480a99dd62e537b9151606e707036c6
jghee <ghj171937@gmail.com>
2021-09-08 17:54:18 +0900

    dp

--- coding/ch8/p_220.py
+++ coding/ch8/p_220.py
@@ -1,16 +1,30 @@
 n = int(input())
 a = list(map(int, input().split()))
-dp = [0] * n
+# 두번째 풀이
+dp1 = [0] * n
+dp2 = [0] * n
+dp1[0], dp1[1] = a[0], a[1]
+dp2[0], dp2[1] = a[0], a[1]
 
-for i in range(0, n):
-    for j in range(i+2, n):
-        dp[i] = max(dp[i], a[i] + a[j])
-        dp[j] = dp[i]
+for i in range(2, n):
+    dp1[i] = dp1[i-2] + a[i]
+    dp2[i] = max(dp1[i], dp2[i-1])
 
+print(dp2[n-1])
+
+# 최초 풀이
+# dp = [0] * n
+#
+# for i in range(0, n):
+#     for j in range(i+2, n):
+#         dp[i] = max(dp[i], a[i] + a[j])
+#         dp[j] = dp[i]
 # print(dp)
 # print(max(dp))
 #
-#
+
+
+# 교재 답안 예시
 # n = int(input())
 # array = list(map(int, input().split()))
 #
